45d139de123e9caff2df163cbe5a230850b14677
Pisa438 <33507807+Pisa438@users.noreply.github.com>
2019-01-20 12:11:58 -0500

    s

--- routes/users.js
+++ routes/users.js
@@ -45,7 +45,7 @@ router.post('/authenticate', (req, res, next) => {
                     success: true,
                     token: 'JWT '+token,
                     user: {
-                        id: user.id,
+                        id: user._id,
                         name: user.name,
                         username: user.username,
                         email: user.email
