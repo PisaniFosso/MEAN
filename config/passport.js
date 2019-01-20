e76d8b3d560d91a98581a5d3bf03cdd2f099015a
Pisa438 <33507807+Pisa438@users.noreply.github.com>
2019-01-20 07:50:03 -0500

    connected

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,5 @@
 const JwtStrategy = require('passport-jwt').Strategy;
-const extractJwt = require('passport-jwt').ExtractJwt;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('../models/user');
 const config = require('../config/database');
 
