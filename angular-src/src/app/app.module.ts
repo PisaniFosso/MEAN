faca450d1c8f89c27360e1739faa633924dd963c
Pisa438 <33507807+Pisa438@users.noreply.github.com>
2019-01-20 15:26:31 -0500

    appRoutes

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
-const appRoute: Routes = [
+const appRoutes: Routes = [
   {path:'', component: HomeComponent},
   {path:'register', component: RegisterComponent},
   {path:'login', component: LoginComponent},
