60587f6740a9ab79ea4bc205666ac062c9ce1a06
Pisa438 <33507807+Pisa438@users.noreply.github.com>
2019-01-20 06:21:22 -0500

    mongo db update

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
 // Register route
-router.get('/register', (req, res, next) => {
-    res.send('Register');
+router.post('/register', (req, res, next) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser (err, user) => {
+        if (err) {
+            res.json({success: false, msg:'Failed to register user'});
+        } else {
+            res.json({success: true, msg:'User registered'});
+        }
+    });
 });
 
 // Authenticate route
-router.get('/authenticate', (req, res, next) => {
+router.post('/authenticate', (req, res, next) => {
     res.send('Authenticate');
 });
 
 // Profile route
 router.get('/profile', (req, res, next) => {
     res.send('Profile');
-});
-
-// Validate route
-router.get('/validate', (req, res, next) => {
-    res.send('Validate');
 });
 
 module.exports = router;
