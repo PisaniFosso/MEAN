eac69a2056866dbc5eee79c207daaeb316b23cf2
Pisa438 <33507807+Pisa438@users.noreply.github.com>
2019-01-20 20:07:59 -0500

    register validate

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ValidateService } from '../../services/validate.service'
+import { FlashMessagesService } from 'angular2-flash-messages';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -11,7 +12,7 @@ export class RegisterComponent implements OnInit {
   email: String;
   phone: String;
   password: String;
-  constructor(private validateService: ValidateService) { }
+  constructor(private validateService: ValidateService, private flashMessagesServices: FlashMessagesService) { }
 
   ngOnInit() {
   }
@@ -26,16 +27,19 @@ export class RegisterComponent implements OnInit {
 
   //Requiered field
   if (!this.validateService.validateRegister(user)) {
+    this.flashMessagesServices.show('please fill in all fields', {cssClass:'alert-danger', timeout:3000});
     return false;
   }
 
   //validate email
   if (!this.validateService.validateEmail(user.email)) {
+    this.flashMessagesServices.show('please Enter a valid email', {cssClass:'alert-danger', timeout:3000});
     return false;
   }
 
   //validate phone
   if (!this.validateService.validatePhone(user.phone)) {
+    this.flashMessagesServices.show('please Enter a valid phone', {cssClass:'alert-danger', timeout:3000});
     return false;
   }
 }
